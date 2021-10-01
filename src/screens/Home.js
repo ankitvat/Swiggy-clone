341569c129537b1ef74531dd591e2dc8a07e6bbd
Ankit Vat <ankitvat874@gmail.com>
2021-10-01 15:02:37 +0530

    Removed previous homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView, Dimensions, Image,FlatList} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, ScrollView, Dimensions, Image,FlatList} from 'react-native';
 import {Settings, Bell} from 'react-native-feather';
 import CustomText from '../components/Text/CustomText';
 import banner1 from '../assets/images/banner1.png';
@@ -40,75 +40,10 @@ export default function Home({navigation: {navigate}}) {
 		);
 	};
   return (
-    <ScrollView
-      contentContainerStyle={styles.parentLayout}
-      showsVerticalScrollIndicator={false}
-      alwaysBounceVertical={false}>
-      <View style={styles.topHeader}>
-        <View style={{flexDirection: 'column', alignItems: 'flex-start'}}>
-          <CustomText variant="h0" bold text="Trending" />
-          <CustomText
-            variant="h0"
-            text="these days."
-            style={{marginTop: '-5%'}}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            marginTop: '7%',
-            width: w / 5,
-            justifyContent: 'space-around',
-          }}>
-          <Bell width={25} height={25} color="#000" />
-          <Settings width={25} height={25} color="#000" />
-        </View>
-      </View>
-			<View style={styles.carouselContainer}>
-				<Carousel 
-					data={banners}
-					inactiveSlideOpacity={1}
-					inactiveSlideScale={1}
-					onBeforeSnapToItem={(index) => setActive(index)}
-					renderItem={renderBanner}
-					sliderWidth={w*0.95}
-					itemWidth={w*0.95}
-					loop={true}
-					enableSnap={true}
-					autoplayInterval={3000}
-					autoplay={true}
-				/>
-				<Pagination 
-					dotsLength={banners.length}
-					activeDotIndex={active}
-					containerStyle={{
-						paddingHorizontal: 0,
-            paddingVertical: 0,
-            marginVertical: '4%',
-					}}
-					dotStyle={{
-            width: scale(8),
-            height: scale(8),
-            backgroundColor: theme.colors.black,
-            borderRadius: scale(4),
-          }}
-          inactiveDotStyle={{
-            backgroundColor: '#000',
-          }}
-				/>
-				</View>
-				<View style = {styles.categories}>
-					<CustomText variant="h1"  text="Shop by Category"  />
-					<CustomText variant = "subtext" text="Curated just for you" style = {{marginTop:'-1%',marginLeft:'0.2%'}} />
-					<FlatList 
-						numColumns={2}
-						data={categories}
-						renderItem={renderCategory}
-						
-						showsVerticalScrollIndicator={false}
-					/>
-				</View>
-    </ScrollView>
+    <SafeAreaView style = {styles.parentLayout}>
+
+		</SafeAreaView>
+
   );
 }
 
@@ -119,52 +54,8 @@ const styles = StyleSheet.create({
     paddingVertical: '5%',
     backgroundColor: '#EBEBEB',
 		flexDirection:'column',
-  },
-  topHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-		position:'absolute',
-    flexShrink: 1,
-		paddingTop:'7%',
-    paddingHorizontal: '5%',
-  },
-	bannerContainer: {
-		width: w*0.95,
-		height: '100%',
-		flexDirection:'column',
-		marginTop:'5%',
-		
-	},
-	carouselContainer:{
-		height:'28%',
-		marginTop:'28%',
+		flex:1
 
-	},
-	banner:{
-		resizeMode:'cover',
-		width:w*0.95,
-		height:'100%',
-		borderRadius:scale(10),
-	},
-	categories:{
-		width:w,
-		height:h,
-		paddingHorizontal:'4%',
-		flexDirection:'column',
-		
-	},
-	category:{
-		
-		flex:1,
-		height:h/4.2,
-		paddingBottom:'5%',
-	},
-	categoryImage:{
-		borderRadius:scale(10),
-		resizeMode:'contain',
-		width:w/2.2,
-		
-		
-	},
+  },
+ 
 });
